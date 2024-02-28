a5cc022cbc9b6b5b3ca7789d61149ad1a20328a6
Kimjaejung <67777124+baebang@users.noreply.github.com>
2024-02-29 03:32:51 +0900

    [Add] 테스트 빌드

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <img src={process.env.PUBLIC_URL + '/img/dsd.png'}  alt="Need Sleep" />
+            <br></br>
+            <text>기분 좋을떄마다 하나씩 올릴게요 즐찾해주세요</text>
+        </div>
+    );
 }
 
 export default App;
