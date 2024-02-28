3968b1aacc43ae64ecc49431c3d3a711d0e6c2ef
Kimjaejung <67777124+baebang@users.noreply.github.com>
2024-02-29 03:38:48 +0900

    [Add] 테스트 커밋

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-
 function App() {
     return (
         <div className="App">
-            <img src={process.env.PUBLIC_URL + '/img/dsd.png'}  alt="Need Sleep" />
+            <img src={process.env.PUBLIC_URL + '/img/dsd.png'} alt="Need Sleep" />
             <br></br>
             <text>기분 좋을떄마다 하나씩 올릴게요 즐찾해주세요</text>
         </div>
