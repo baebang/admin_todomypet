bc3f87bff7b1bbb949a32df303997a1b54726dc5
Kimjaejung <67777124+baebang@users.noreply.github.com>
2024-02-29 03:44:19 +0900

    [Add] Test commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
         <div className="App">
             <img src={process.env.PUBLIC_URL + '/img/dsd.png'} alt="Need Sleep" />
             <br></br>
-            <text>기분 좋을떄마다 하나씩 올릴게요 즐찾해주세요.</text>
+            <text>기분 좋을떄마다 하나씩 올릴게요 즐찾해주세요!</text>
         </div>
     );
 }
